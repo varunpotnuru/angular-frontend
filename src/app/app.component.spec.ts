13a8d9bd7b181ba4c81e22484007d79ad1a02d55
varun <varunpotnuru123@gmail.com>
2020-05-07 09:02:14 +0530

    initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'springangular'`, () => {
+  it(`should have as title 'angularintegrationwithspring'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('springangular');
+    expect(app.title).toEqual('angularintegrationwithspring');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('springangular app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('angularintegrationwithspring app is running!');
   });
 });
